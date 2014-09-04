18ee39d3965b099f0bbca5f4f165016b5493438e
dhilipsiva <dhilipsiva@gmail.com>
2014-09-04 13:34:30 +0530

    Write a proper flash message helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,25 @@
 module ApplicationHelper
-  def flash_messages(opts = {})
-    ignore_types = ["order_completed"].concat(Array(opts[:ignore_types]).map(&:to_s) || [])
-
-    flash.each do |msg_type, text|
-      unless ignore_types.include?(msg_type)
-        concat(content_tag :div, text, class: "alert alert-#{msg_type}")
+end
+module Spree
+  module BaseHelper
+    def bs_alert_convert msg_type
+      # Convers to boorstrap equivalent
+      case msg_type
+      when "error"
+        return "danger"
       end
+      return "info"
     end
-    nil
+    def flash_messages(opts = {})
+      ignore_types = ["order_completed"].concat(Array(opts[:ignore_types]).map(&:to_s) || [])
+
+      flash.each do |msg_type, text|
+        unless ignore_types.include?(msg_type)
+          concat(content_tag :div, text, class: "alert alert-#{bs_alert_convert msg_type}")
+        end
+      end
+      nil
+    end
   end
 end
+
