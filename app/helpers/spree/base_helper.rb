4278cf2b9a00b7544503ed9096df1dc89a5bfd71
dhilipsiva <dhilipsiva@gmail.com>
2014-09-04 13:21:29 +0530

    Modify flash messages helper

---
+++ app/helpers/spree/base_helper.rb
@@ -0,0 +1,14 @@
+module Spree
+  module BaseHelper
+    def flash_messages(opts = {})
+      ignore_types = ["order_completed"].concat(Array(opts[:ignore_types]).map(&:to_s) || [])
+
+      flash.each do |msg_type, text|
+        unless ignore_types.include?(msg_type)
+          concat(content_tag :div, text, class: "alert alert-#{msg_type}")
+        end
+      end
+      nil
+    end
+  end
+end
