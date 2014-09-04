d43a2ea6725e072d69b67582b915a32329d78d59
dhilipsiva <dhilipsiva@gmail.com>
2014-09-04 11:05:33 +0530

    Enhance checkout progress

---
+++ app/helpers/spree/checkout_helper.rb
@@ -0,0 +1,32 @@
+module Spree
+  module CheckoutHelper
+    def checkout_states
+      @order.checkout_steps
+    end
+
+    def font_class state
+      case state
+      when "address"
+        "map-marker"
+      when "delivery"
+        "truck"
+      when "payment"
+        "money"
+      when "complete"
+        "check"
+      end
+    end
+
+    def checkout_progress
+      states = checkout_states
+      items = states.map do |state|
+        text = Spree.t("order_state.#{state}").titleize
+        css_class = "col-lg-3 col-md-3 col-sm-6 col-xs-12 #{state}"
+        css_class += ' current' if state == @order.state
+        icon = fa_icon font_class state
+        content_tag('li', content_tag('span', icon + text), class: css_class)
+      end
+      raw(items.join("\n"))
+    end
+  end
+end
