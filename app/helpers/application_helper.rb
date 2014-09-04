150a8a6cf905f829f448c3835f6c76bb3ec69935
dhilipsiva <dhilipsiva@gmail.com>
2014-09-04 13:27:49 +0530

    Buffix helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_messages(opts = {})
+    ignore_types = ["order_completed"].concat(Array(opts[:ignore_types]).map(&:to_s) || [])
+
+    flash.each do |msg_type, text|
+      unless ignore_types.include?(msg_type)
+        concat(content_tag :div, text, class: "alert alert-#{msg_type}")
+      end
+    end
+    nil
+  end
 end
